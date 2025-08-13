7ee39abcdec83fe717f5dc3c73d60ae5ae1321be
Android Studio <devandroid@ort>
2025-08-13 09:35:00 -0300

    ghPages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'TP8-EFSI',
-  homepage: "https://joaacoordonez.github.io/TP8-EFSI"
+  base: '/TP8-EFSI/'
 })
